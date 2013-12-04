bcb57ec1a8ddd031e0bab6090a8ecb1cbf99009b
Abe & Kimberly <abe+kim@appacademy.io>
2013-12-04 17:05:10 -0500

    handles submit on photos view

--- app/assets/javascripts/views/photo_from_view.js
+++ app/assets/javascripts/views/photo_from_view.js
@@ -3,14 +3,23 @@
 
   var PhotoFormView = PT.PhotoFormView = function () {
     this.$el = $('<div></div>');
+    this.$el.on("submit", this.submit); //come back to this
   }
 
   _.extend(PhotoFormView.prototype, {
     render: function () {
       this.$el.empty();
-      console.log($('#new-photo-form'))
       this.$el.append(JST["photo_form"]());
       return this; //why not return this.el???
+    },
+
+    submit: function (event) {
+      event.preventDefault();
+      console.log(event);
+      var newPhotoObj = $('#new-photo-form').serializeJSON();
+
+      var newPhoto = new PT.Photo(newPhotoObj["photo"]);
+      newPhoto.create(function(){});
     }
 
   });
